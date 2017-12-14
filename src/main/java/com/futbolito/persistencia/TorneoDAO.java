7e2642d81f713ac0e36cb907d97cb2ad90e665fa
Camiloinostroza9 <caminost@alumnos.ubiobio.cl>
2017-12-13 22:35:47 -0300

    arreglo

--- src/main/java/com/futbolito/persistencia/TorneoDAO.java
+++ src/main/java/com/futbolito/persistencia/TorneoDAO.java
@@ -18,12 +18,12 @@ public class TorneoDAO {
 	private static final String READ_PROPIOS = "select * from torneo where idUsuario=?";
 	private static final String INSERT_QUERY="insert into torneo (Nombre,FechaInicio,numeroParticipantes,idCampeon,idUsuario) values (?,?,?,?,?)";
 	private static final String READ_TORNEO = "select * from cancha where idTorneo=?";
-<<<<<<< HEAD
-	private static final String BUSCAR_POR_ID = "select numeroParticipantes from torneo where `idTorneo`=?";
+
+	private static final String BUSCAR_POR_ID2 = "select numeroParticipantes from torneo where `idTorneo`=?";
 	private static final String ACTUALIZA_CANTEQUI = "UPDATE `torneo` SET `numeroParticipantes`=? WHERE `idTorneo`=?";
-=======
+
 	private static final String BUSCAR_POR_ID = "select * from torneo where idTorneo=?";
->>>>>>> 667d60775c2fa5a53ae35dd6bc48c36faafd22c0
+
 	//private static final String UPDATE_ESTADO_CANCHA = "UPDATE cancha SET Estado = ? WHERE idCancha = ?";
     private static final String DB_NAME = "futbolito";
     private static final String PORT="3306";
@@ -165,7 +165,7 @@ public class TorneoDAO {
         Connection conn=null;
         
         TorneoTO torneito = new TorneoTO();
-        torneito = buscarPorId(idTorneo);
+        torneito = buscarPorId2(idTorneo);
         int valor = torneito.getNumeroParticipantes();
         int numeroPar = valor+1;
        try{
@@ -188,13 +188,13 @@ public class TorneoDAO {
    	
    }
     
-public TorneoTO buscarPorId(int id) throws SQLException{
+public TorneoTO buscarPorId2(int id) throws SQLException{
         
         TorneoTO result = null;
         Connection conn=null;
         try {
             conn = getConnection();
-            PreparedStatement ps = conn.prepareStatement(BUSCAR_POR_ID);
+            PreparedStatement ps = conn.prepareStatement(BUSCAR_POR_ID2);
             ps.setInt(1,id);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
